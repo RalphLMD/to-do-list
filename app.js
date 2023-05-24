70db4c340d2496db658154fa44ba9e23e991dcea
Ralph Dahilig <ralphlance11@gmail.com>
2023-05-24 18:18:15 +0800

    Initial Commit

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+//mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb+srv://ralphlance11:h7M3Ucm0TIJeCWmo@cluster0.ln7cqa9.mongodb.net/todolistDB")
 
 const listItemSchema = new mongoose.Schema({
     name: String
